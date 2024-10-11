bdd0274280a92248875750be17828d2c53efc851
mehedi1022 <mahdibinalamgir@gmail.com>
2024-10-11 15:00:03 +0600

    added alert return

--- script.js
+++ script.js
@@ -56,10 +56,7 @@ for (const btn of allBtn) {
     })
 }
 
-document.getElementById('btn-delete').addEventListener('click', function(){
-    const secret = document.getElementById('secret-info');
-    secret.style.display = 'none';
-})
+
 
 function updateGrandTotal(status) {
     const totalCost = getConvertedValue("total-cost");
@@ -70,14 +67,22 @@ function updateGrandTotal(status) {
         const couponCode = document.getElementById("coupon-code").value;
         let discounted;
         if (couponCode == "NEW15" || couponCode == "Couple 20") {
+
             if (couponCode == "NEW15") {
                 discounted = totalCost * .15;
             } else {
                 discounted = totalCost * .2;
             }
+
+            document.getElementById('btn-delete').addEventListener('click', function () {
+                const secret = document.getElementById('secret-info');
+                secret.style.display = 'none';
+            })
+
             document.getElementById("grand-total").innerText = totalCost - discounted;
         } else {
             alert("Please enter valid coupon code");
+            return;
         }
     }
 
